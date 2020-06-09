b31a897970da65df856a566424dc4f085d21e36c
Gopala Gonuguntla <gopal@Gopalas-MacBook-Pro.local>
2020-06-08 19:17:51 -0500

    Initial commit

---
+++ src/main/java/com/example/github/events/GithubEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.github.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github/events/GithubEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
